75077d3c4cfabfe3ebfd0f452edd2d76504aa9a8
Teebone Ding <teebone_ding@trend.com.tw>
2016-06-02 15:32:30 +0800

    Add screen settings together

--- install_my_setting.sh
+++ install_my_setting.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
+echo "Copy vimrc into your home directory..."
 cp vimrc ~/.vimrc
+echo "Copy screenrc into your home directory..."
+cp screenrc ~/.screenrc
 # Need to check whether ~/.vim exist first
+echo "Copy .vim/ dir to your home directory..."
 cp -r vim/ ~/
 rm -rf ~/.vim
 mv ~/vim/ ~/.vim
+echo "Finished!"
